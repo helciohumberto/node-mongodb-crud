e1ec5b618058844f95c6c5bc9f554149129d681a
helciohumberto <helciohumberto@gmail.com>
2025-03-22 21:01:29 -0300

    customer.ejs: Front de cadastro

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db')
+const db = require('../db');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -9,7 +9,11 @@ router.get('/', (req, res, next) => {
       console.log(customers)
       res.render("index", {title: "Express", customers})
     })
-    .catch(error => console.log(error))
-})
+    .catch(error => console.log(error));
+});
+
+router.get('/new', (req, res) => {
+  res.render("customer", {title: "Cadastro de Cliente"});
+});
 
 module.exports = router;
